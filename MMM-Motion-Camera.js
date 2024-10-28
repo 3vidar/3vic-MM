fc49be0bf5d698759ae6cb1f7b06dfbab0851f91
3vidar <trividar@gmail.com>
2024-10-28 21:15:10 +0100

    Update MMM-Motion-Camera.js

--- MMM-Motion-Camera.js
+++ MMM-Motion-Camera.js
@@ -1,44 +1,22 @@
-/* MagicMirror Module: MMM-Motion-Camera */
 Module.register("MMM-Motion-Camera", {
   defaults: {
-    timeout: 30, // Zeit in Sekunden bis der Bildschirm ausgeschaltet wird
+      inactivityTimeout: 30 // Sekunden
   },
 
-  start: function () {
-    this.motionDetected = false;
-    this.screenIsOn = true;
-    this.sendSocketNotification("START_MOTION_DETECTION", this.config);
+  start: function() {
+      Log.info("Starting module: " + this.name);
+      this.screenOn = true;
+      this.sendSocketNotification("INIT_MOTION_DETECTION", this.config.inactivityTimeout);
   },
 
-  socketNotificationReceived: function (notification, payload) {
-    if (notification === "MOTION_DETECTED") {
-    this.motionDetected = true;
-    if (!this.screenIsOn) {
-      const exec = require('child_process').exec;
-      exec('caffeinate -u -t 1', (err) => {
-        if (err) {
-          console.error('Fehler beim Einschalten des Bildschirms:', err);
-        } else {
-          this.sendNotification("SCREEN_WAKEUP");
-          console.log("Bildschirm eingeschaltet wegen Bewegung");
-        }
-      });
-        this.sendNotification("SCREEN_WAKEUP");
-          console.log("Bildschirm eingeschaltet wegen Bewegung");
-          this.screenIsOn = true;
+  socketNotificationReceived: function(notification, payload) {
+      if (notification === "SCREEN_ON") {
+          this.show();
+          this.screenOn = true;
+      } else if (notification === "SCREEN_OFF") {
+          this.hide();
+          this.screenOn = false;
       }
-      console.log("Bewegung erkannt");
-    } else if (notification === "NO_MOTION_DETECTED") {
-      this.motionDetected = false;
-      console.log("Keine Bewegung erkannt");
-      setTimeout(() => {
-        if (!this.motionDetected && this.screenIsOn) {
-          console.log('Bildschirm wird ausgeschaltet');
-          console.log("Bildschirm ausschalten");
-          this.screenIsOn = false;
-          this.sendSocketNotification("START_MOTION_DETECTION", this.config); // Erneutes Starten der Bewegungserkennung nach dem Ausschalten des Bildschirms
-        }
-      }, this.config.timeout * 1000);
-    }
   }
 });
+
