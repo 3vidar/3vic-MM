38f5bfe7703c5f5fd06edae89b6977752e92cd96
3vidar <trividar@gmail.com>
2024-10-28 21:16:02 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,58 +1,36 @@
-// NodeHelper: node_helper.js
-const NodeHelper = require('node_helper');
-const PythonShell = require('python-shell');
+const NodeHelper = require("node_helper");
+const { spawn } = require("child_process");
 
 module.exports = NodeHelper.create({
-  start: function () {
-    this.started = false;
-  },
+    start: function() {
+        console.log("Starting node helper for: " + this.name);
+        this.motionDetectionProcess = null;
+    },
 
-  socketNotificationReceived: function (notification, payload) {
-    if (notification === "START_MOTION_DETECTION") {
-      this.config = payload;
-      this.startMotionDetection();
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "INIT_MOTION_DETECTION") {
+            this.startMotionDetection(payload);
+        }
+    },
+
+    startMotionDetection: function(timeout) {
+        if (this.motionDetectionProcess) {
+            this.motionDetectionProcess.kill();
+        }
+
+        this.motionDetectionProcess = spawn("python3", ["/mnt/data/motion_detection.py", timeout]);
+
+        this.motionDetectionProcess.stdout.on("data", (data) => {
+            const message = data.toString().trim();
+            if (message === "MOTION_DETECTED") {
+                this.sendSocketNotification("SCREEN_ON");
+            } else if (message === "NO_MOTION") {
+                this.sendSocketNotification("SCREEN_OFF");
+            }
+        });
+
+        this.motionDetectionProcess.on("close", (code) => {
+            console.log("Motion detection process exited with code " + code);
+        });
     }
-  },
-
-  startMotionDetection: function () {
-    const pythonOptions = {
-      mode: 'text',
-      pythonOptions: ['-u'],
-      scriptPath: 'modules/MMM-Motion-Camera',
-      args: [this.config.timeout]
-    };
-
-    if (this.pythonShell) {
-      this.pythonShell.end((err) => {
-        if (err) console.error('Fehler beim Stoppen des Python-Skripts:', err);
-      });
-    }
-
-    this.pythonShell = new PythonShell.PythonShell('motion_detection.py', pythonOptions);
-
-    this.pythonShell.on('message', (message) => {
-      if (message === 'MOTION_DETECTED') {
-        this.sendSocketNotification('MOTION_DETECTED');
-      } else if (message === 'NO_MOTION_DETECTED') {
-        this.sendSocketNotification('NO_MOTION_DETECTED');
-      }
-    });
-
-    this.pythonShell.on('error', (err) => {
-      console.error('Fehler beim Ausführen des Motion-Skripts:', err);
-    });
-
-    this.pythonShell.on('close', () => {
-      console.log('Python-Skript beendet, Bewegungserkennung wird neu gestartet');
-      this.startMotionDetection();
-    });
-  },
-
-  stop: function () {
-    if (this.pythonShell) {
-      this.pythonShell.end((err) => {
-        if (err) console.error('Fehler beim Stoppen des Python-Skripts:', err);
-      });
-    }
-  }
 });
